26408b9a47247be3004c06f5cc9917627d62da3c
Administrador <Administrador@10.4.128.109>
2015-10-28 10:07:04 -0200

    COMMIT EVENTO IMPLEMENTS

---
+++ src/main/java/controller/Evento.java
@@ -0,0 +1,71 @@
+package controller;
+
+import java.util.Date;
+
+import org.primefaces.model.ScheduleEvent;
+
+public class Evento implements ScheduleEvent {
+
+	@Override
+	public Object getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getEndDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getStartDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStyleClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAllDay() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEditable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setId(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
